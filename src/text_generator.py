9c2058b3991e65e4dc28624d6c30cc8c39edd80e
mmmity <baidurik.media.official@gmail.com>
2024-03-23 17:05:15 +0300

    Fixed deque slicing

--- src/text_generator.py
+++ src/text_generator.py
@@ -2,8 +2,9 @@ import abc
 import typing
 import random
 import collections
+import itertools
 
-from exceptions import EndOfFile
+from src.exceptions import EndOfFile
 
 
 class TextGenerator(abc.ABC):
@@ -40,12 +41,13 @@ class RandomTextGenerator(TextGenerator):
     Vocabulary from file, words are separated by whitespace characters.
     Has a pool of randomly generated words.
     '''
-    def __init__(self, file: typing.TextIO, init_poolsize: int):
+    def __init__(self, filename: str, init_poolsize: int):
         '''
         Initializes the vocabulary with words from file.
         Initializes the pool with init_poolsize random words.
         '''
-        self.vocab = file.read().split()
+        with open(filename, 'r') as file:
+            self.vocab = file.read().split()
         self.__poolsize = init_poolsize * 2 - 1
         self.__pool = collections.deque()
 
@@ -72,18 +74,19 @@ class RandomTextGenerator(TextGenerator):
         Returns num_words words from pool before current word.
         If num_words is greater than available amount, returns all.
         '''
-        word_index = (self.__poolsize + 1) // 2
+        word_index = len(self.__pool) - (self.__poolsize + 1) // 2
         num_words = max(num_words, self.__poolsize // 2)
-        return self.__pool[:word_index]
+        return list(itertools.islice(self.__pool, word_index))
 
     def words_after(self, num_words: int) -> typing.List[str]:
         '''
         Returns num_words words from pool after current word.
         If num_words is greater than available amount, returns all.
         '''
-        word_index = (self.__poolsize + 1) // 2
+        word_index = len(self.__pool) - (self.__poolsize + 1) // 2
         num_words = max(num_words, self.__poolsize // 2)
-        return self.__pool[word_index+1:]
+        return list(itertools.islice(self.__pool,
+                                     word_index+1, len(self.__pool)))
 
 
 class FileTextGenerator(TextGenerator):
@@ -91,11 +94,12 @@ class FileTextGenerator(TextGenerator):
     Text generator that returns continuous words from text file.
     When no more words are left, raises EndOfFile.
     '''
-    def __init__(self, file: typing.TextIO):
+    def __init__(self, filename: str):
         '''
         Initializes text with text from file, split into words.
         '''
-        self.text = file.read().split()
+        with open(filename, 'r') as file:
+            self.text = file.read().split()
         self.__index = 0
 
     def next_word(self):
@@ -117,7 +121,8 @@ class FileTextGenerator(TextGenerator):
         If num_words is greater than available amount, returns all.
         '''
         num_words = max(num_words, self.__index)
-        return self.text[self.__index - num_words:self.__index]
+        return list(itertools.islice(self.text,
+                                     self.__index - num_words, self.__index))
 
     def words_after(self, num_words: int) -> str:
         '''
@@ -125,5 +130,5 @@ class FileTextGenerator(TextGenerator):
         If num_words is greater than available amount, returns all.
         '''
         num_words = max(num_words, len(self.text) - self.__index - 1)
-        return self.text[self.__index + 1:
-                         self.__index + num_words + 1]
+        return list(itertools.islice(self.text, self.__index + 1,
+                                     self.__index + num_words + 1))
