c65b32f0112bd6111624603fe2e80fed2ba7d7cf
mmmity <baidurik.media.official@gmail.com>
2024-03-30 00:22:42 +0300

    Fixed codestyle issues

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,8 @@ from tests.test_text_generator import TestRandomTextGenerator, \
                                       TestFileTextGenerator
 from tests.test_statistics import TestStatistics, TestFileStatistics
 from tests.test_text_overseer import TestTextOverseer
-from tests.test_widgets import TestButton, TestTextInput, TestSwitch, TestNumberInput
+from tests.test_widgets import TestButton, TestTextInput, \
+                               TestSwitch, TestNumberInput
 import unittest
 
 import sys
