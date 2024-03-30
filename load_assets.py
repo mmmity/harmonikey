345d78853bddd76916063355f6fdc4067e8a27e7
mmmity <baidurik.media.official@gmail.com>
2024-03-30 17:28:24 +0300

    Added script for loading assets

---
+++ load_assets.py
@@ -0,0 +1,9 @@
+import requests as req
+
+top10000_eng_long = req.get('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears-long.txt')
+with open('assets/vocabs/top10000_english_long.txt', 'w') as voc_file:
+    voc_file.write(top10000_eng_long.text)
+
+top1000_eng = req.get('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt')
+with open('assets/vocabs/top1000_english.txt', 'w') as voc_file:
+    voc_file.write(top1000_eng.text)
