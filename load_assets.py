954d19012df913ba9a94437097f526b7a737100b
mmmity <baidurik.media.official@gmail.com>
2024-03-30 17:33:31 +0300

    Added creation of stats file

--- load_assets.py
+++ load_assets.py
@@ -1,4 +1,5 @@
 import requests as req
+import os
 
 top10000_eng_long = req.get('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears-long.txt')
 with open('assets/vocabs/top10000_english_long.txt', 'w') as voc_file:
@@ -7,3 +8,10 @@ with open('assets/vocabs/top10000_english_long.txt', 'w') as voc_file:
 top1000_eng = req.get('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt')
 with open('assets/vocabs/top1000_english.txt', 'w') as voc_file:
     voc_file.write(top1000_eng.text)
+
+try:
+    os.makedirs('stats')
+except FileExistsError:
+    pass
+
+open('stats/stats.csv', 'a').close()
